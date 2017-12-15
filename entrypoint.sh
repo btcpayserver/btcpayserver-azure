e7301111bdd6a78f91083010dcc2b5e70bbe6960
nicolas.dorier <nicolas.dorier@gmail.com>
2017-12-15 12:42:34 +0900

    install docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
 
-# Remove error message from apt-get
+# Install docker (https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#set-up-the-repository) and docker-compose 
 sudo apt-get update 2>error
-sudo apt-get install -y git curl 2>error
+sudo apt-get install -y \
+    git \
+    curl \
+    apt-transport-https \
+    ca-certificates \
+    software-properties-common \
+    2>error
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0-rc2/docker-compose-1.16.1 -o /usr/local/bin/docker-compose
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 git clone https://github.com/btcpayserver/btcpayserver-docker && cd btcpayserver-docker/Regtest
