3006e4a30d059266078222d3f2efd6a2a27f1bb2
nicolas.dorier <nicolas.dorier@gmail.com>
2017-12-15 11:54:07 +0900

    update typehandler remove an error on apt-get

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+# Remove error message from apt-get (https://serverfault.com/questions/500764/dpkg-reconfigure-unable-to-re-open-stdin-no-file-or-directory)
+sudo dpkg-preconfigure -f noninteractive -p critical
+sudo dpkg --configure -a
+
 sudo apt-get update
 sudo apt-get install -y git
 git clone https://github.com/btcpayserver/btcpayserver-docker && cd btcpayserver-docker/Regtest
