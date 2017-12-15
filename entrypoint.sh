9566bf276d0890675b3fed36aad9d51ccbfed525
nicolas.dorier <nicolas.dorier@gmail.com>
2017-12-15 12:24:18 +0900

    Remove outdated docker plugin

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 
-# Remove error message from apt-get (https://serverfault.com/questions/500764/dpkg-reconfigure-unable-to-re-open-stdin-no-file-or-directory)
-sudo dpkg-preconfigure -f noninteractive -p critical
-sudo dpkg --configure -a
+# Remove error message from apt-get
+sudo apt-get update 2>error
+sudo apt-get install -y git curl 2>error
 
-sudo apt-get update
-sudo apt-get install -y git
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0-rc2/docker-compose-1.16.1 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 git clone https://github.com/btcpayserver/btcpayserver-docker && cd btcpayserver-docker/Regtest
 docker-compose up
