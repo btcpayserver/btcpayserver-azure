c06dcab4bb9c0cfbf42fd7342af0959725f2959c
nicolas.dorier <nicolas.dorier@gmail.com>
2017-12-15 12:53:33 +0900

    forgot -y

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,10 +19,12 @@ sudo add-apt-repository \
    stable"
 
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce
 
+# Install docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Clone btcpayserver
 git clone https://github.com/btcpayserver/btcpayserver-docker && cd btcpayserver-docker/Regtest
 docker-compose up
