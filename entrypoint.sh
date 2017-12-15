1099c912627fc3e2a277ba9139889064605738d5
nicolas.dorier <nicolas.dorier@gmail.com>
2017-12-15 22:55:19 +0900

    pass the dns name to the docker vm

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# It is running as root
+export DNS_NAME=$1
+
+echo "DNS NAME: $DNS_NAME"
+echo "DNS_NAME=$DNS_NAME" >> /etc/environment
 
 # Install docker (https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#set-up-the-repository) and docker-compose 
-sudo apt-get update 2>error
-sudo apt-get install -y \
+apt-get update 2>error
+apt-get install -y \
     git \
     curl \
     apt-transport-https \
@@ -11,19 +16,19 @@ sudo apt-get install -y \
     software-properties-common \
     2>error
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get update
-sudo apt-get install -y docker-ce
+apt-get update
+apt-get install -y docker-ce
 
 # Install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # Clone btcpayserver
 git clone https://github.com/btcpayserver/btcpayserver-docker && cd btcpayserver-docker/Regtest
