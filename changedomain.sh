9023010a615f4a5e452e57bc105f23de976cc76f
nicolas.dorier <nicolas.dorier@gmail.com>
2018-01-13 01:15:38 +0900

    Remove unused line in environment file when change domain

--- changedomain.sh
+++ changedomain.sh
@@ -6,7 +6,9 @@ echo "Changing domain to \"$NEW_HOST\""
 export BTCPAY_HOST="$NEW_HOST"
 export ACME_CA_URI="https://acme-v01.api.letsencrypt.org/directory"
 
-# .env file, last entry win, so just add to the file
+# Modify environment file
+sed -i '/^BTCPAY_HOST/d' $BTCPAY_ENV_FILE
+sed -i '/^ACME_CA_URI/d' $BTCPAY_ENV_FILE
 echo "BTCPAY_HOST=$BTCPAY_HOST" >> $BTCPAY_ENV_FILE
 echo "ACME_CA_URI=$ACME_CA_URI" >> $BTCPAY_ENV_FILE
 
