7c825596dc847cf529e1b7417bd39258e190b64a
nicolas.dorier <nicolas.dorier@gmail.com>
2018-03-21 16:48:09 +0900

    fix btcpay-update

--- btcpay-update.sh
+++ btcpay-update.sh
@@ -5,5 +5,5 @@ docker-compose -f $BTCPAY_DOCKER_COMPOSE down
 docker-compose -f $BTCPAY_DOCKER_COMPOSE up -d
 
 cd btcpayserver-docker
-find `pwd`/btcpayserver-docker -name "*.sh" -exec chmod +x {} \; 2>/dev/null
-find `pwd`/btcpayserver-docker -name "*.sh" -exec ln -s {} /usr/bin \; 2>/dev/null
+find `pwd` -name "*.sh" -exec chmod +x {} \; 2>/dev/null
+find `pwd` -name "*.sh" -exec ln -s {} /usr/bin \; 2>/dev/null
