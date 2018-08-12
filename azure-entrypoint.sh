531744947386970e5280c8b47f0ab3c8ad160943
nicolas.dorier <nicolas.dorier@gmail.com>
2018-08-13 01:09:23 +0900

    Allow custom SSH Key, remove the need for SSH user/password

--- azure-entrypoint.sh
+++ azure-entrypoint.sh
@@ -17,9 +17,29 @@ fi
 : "${BTCPAYGEN_REVERSEPROXY:=nginx}"
 : "${ACME_CA_URI:=https://acme-staging.api.letsencrypt.org/directory}"
 
+CUSTOM_SSH_KEY="${10}"
+
+# Setup SSH access via private key
+ssh-keygen -t rsa -f /root/.ssh/id_rsa_btcpay -q -P ""
+echo "# Key used by BTCPay Server" >> /root/.ssh/authorized_keys
+cat /root/.ssh/id_rsa_btcpay.pub >> /root/.ssh/authorized_keys
+if [[ "$CUSTOM_SSH_KEY" ]]; then
+    echo "" >> /root/.ssh/authorized_keys
+    echo "# User key" >> /root/.ssh/authorized_keys
+    echo "$CUSTOM_SSH_KEY" >> /root/.ssh/authorized_keys
+    echo "Custom SSH Key added to /root/.ssh/authorized_keys"
+fi
+# sed -i -e '/^PasswordAuthentication / s/ .*/ no/' /etc/ssh/sshd_config
+# userdel -r -f temp
+
+# Configure BTCPAY to have access to SSH
+BTCPAY_HOST_SSHKEYFILE=/root/.ssh/id_rsa_btcpay
+
 # Clone btcpayserver-docker
 git clone $BTCPAY_DOCKER_REPO
 cd btcpayserver-docker
 git checkout $BTCPAY_DOCKER_REPO_BRANCH
 
 . ./btcpay-setup.sh -i
+
+nohup /etc/init.d/sshd restart &
